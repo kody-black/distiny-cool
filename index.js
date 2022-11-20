6ad77fd47d3eaba4713245860c554efe00e9392e
Kody Black <2442658787@qq.com>
2022-11-20 15:42:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const thisYear = new Date().getFullYear()
 const startTimeOfThisYear = new Date(`${thisYear}-01-01T00:00:00+00:00`).getTime()
 const endTimeOfThisYear = new Date(`${thisYear}-12-31T23:59:59+00:00`).getTime()
@@ -15,12 +16,7 @@ function generateProgressBar() {
 
 const readme = `\
 ### Hi there 👋
-
 ⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
-
 ---
-
 ⏰ Updated on ${new Date().toUTCString()}
-
-
-console.log(readme)
+---
