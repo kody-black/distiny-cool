a595b97b4941443532cfdc093268ac7e27270693
Kody Black <2442658787@qq.com>
2022-11-20 15:51:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -15,10 +15,14 @@ function generateProgressBar() {
 
 const readme = `\
 ### Hi there 👋
+
 ⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
+
 ---
+
 ⏰ Updated on ${new Date().toUTCString()}
-![Progress Bar CI](https://github.com/distiny-cool/distiny-cool/workflows/Progress%20Bar%20CI/badge.svg)\
+
+![Progress Bar CI](https://github.com/liununu/liununu/workflows/Progress%20Bar%20CI/badge.svg)\
 `
 
 console.log(readme)
